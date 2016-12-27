23f3898403c69e4b6c87169365cc6acb29f51b2e
Yashodhanvivek <yashodhan.vivek@gmail.com>
2016-12-27 15:56:11 +0530

    Add files via upload

--- predict.m
+++ predict.m
@@ -1,24 +1,48 @@
-function p = predict(theta, X)
-%PREDICT Predict whether the label is 0 or 1 using learned logistic 
-%regression parameters theta
-%   p = PREDICT(theta, X) computes the predictions for X using a 
-%   threshold at 0.5 (i.e., if sigmoid(theta'*x) >= 0.5, predict 1)
+function p = predict(Theta1, Theta2, X)
+%PREDICT Predict the label of an input given a trained neural network
+%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+%   trained weights of a neural network (Theta1, Theta2)
 
-m = size(X, 1); % Number of training examples
+% Useful values
+m = size(X, 1);
+num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly
-p = zeros(m, 1);
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned logistic regression parameters. 
-%               You should set p to a vector of 0's and 1's
+%               your learned neural network. You should set p to a 
+%               vector containing labels between 1 to num_labels.
 %
+% Hint: The max function might come in useful. In particular, the max
+%       function can also return the index of the max element, for more
+%       information see 'help max'. If your examples are in rows, then, you
+%       can use max(A, [], 2) to obtain the max for each row.
+%
+% Theta1 has size 25 x 401
+% Theta2 has size 10 x 26
+% p has size 5000 x 1
 
-p = sigmoid(X * theta) >= 0.5;
+% Input Layer
+a1 = [ones(m, 1), X]; % results in [5000, 401]
 
+z2 = Theta1 * a1'; % results in [25, 5000]
+
+% Hidden Layer
+a2 = sigmoid(z2);  % results in [25, 5000]
+
+a2 = [ones(1, size(a2, 2)); a2]; % results in [26, 5000]
+
+% Output layer
+z3 = Theta2 * a2; % results in [10, 5000]
+
+a3 = sigmoid(z3); % results in [10, 5000]
+
+% calculating max on the transpose of a3 so the index 
+% result, p, has the expected dimensions [5000, 1]
+[val, p] = max(a3', [], 2);
 
 % =========================================================================
-
 
 end
